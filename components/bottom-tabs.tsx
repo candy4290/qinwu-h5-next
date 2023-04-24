30715013b213b26d5abdef892d72bcc30aa71ce4
陈小祥 <xiaoxiang930601@163.com>
2023-04-24 10:54:02 +0800

    feat: 水印

--- components/bottom-tabs.tsx
+++ components/bottom-tabs.tsx
@@ -1,4 +1,4 @@
-import { TabBar } from 'antd-mobile';
+import { TabBar, WaterMark } from 'antd-mobile';
 import Image from 'next/image';
 import { usePathname, useRouter } from 'next/navigation';
 import { useUserInfo } from '@/utils/hooks/use-userInfo';
@@ -7,24 +7,35 @@ import _ from 'lodash';
 export default function BottomTabs() {
     const pathname = usePathname();
     const router = useRouter();
-    const {tabs} = useUserInfo();
+    const {tabs, userInfo} = useUserInfo();
 
     function go(path: string) {
         router.push(path);
     }
 
     return (
-        <TabBar className='tBottom' activeKey={(pathname || '').slice(5)} onChange={value => go(`/home${value}`)}>
-            {tabs.map(item => (
-                <TabBar.Item
-                key={item.key}
-                icon={(pathname || '').slice(5).startsWith(item.key) ? 
-                <Image width={32} height={32} src={item.icon2} alt='' />
-                 : <Image src={item.icon} width={32} height={32} alt='' />}
-                title={item.title}
-                badge={item.badge}
+        <>
+            <TabBar className='tBottom' activeKey={(pathname || '').slice(5)} onChange={value => go(`/home${value}`)}>
+                {tabs.map(item => (
+                    <TabBar.Item
+                    key={item.key}
+                    icon={(pathname || '').slice(5).startsWith(item.key) ? 
+                    <Image width={32} height={32} src={item.icon2} alt='' />
+                    : <Image src={item.icon} width={32} height={32} alt='' />}
+                    title={item.title}
+                    badge={item.badge}
+                    />
+                ))}
+            </TabBar>
+            {
+                userInfo &&
+                <WaterMark
+                    fontColor={'rgba(0,0,0,.06)'}
+                    rotate={-18}
+                    content={userInfo.perName + ' ' + userInfo.perCode}
+                    fullPage={true}
                 />
-            ))}
-        </TabBar>
+            }
+        </>
     )
 }
